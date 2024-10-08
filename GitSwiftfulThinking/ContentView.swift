b732b9440a39ab9b13e889e2ec42e58733e2270a
Shakir Gamzaev <shkir100@gmail.com>
2024-10-08 12:06:35 +0200

    add Image and text swiftful

--- GitSwiftfulThinking/ContentView.swift
+++ GitSwiftfulThinking/ContentView.swift
@@ -10,12 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: "heart.fill")
+                .font(.largeTitle)
+                .foregroundStyle(.blue)
+            Text("Swiftful!")
+            
         }
-        .padding()
     }
 }
 
