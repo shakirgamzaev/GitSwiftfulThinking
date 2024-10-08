20d72da3b0d62f3c61560d449a1ec4cb81f05d93
Shakir Gamzaev <shkir100@gmail.com>
2024-10-08 12:01:44 +0200

    Initial Commit

---
+++ GitSwiftfulThinking/GitSwiftfulThinkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitSwiftfulThinkingApp.swift
+//  GitSwiftfulThinking
+//
+//  Created by shakir Gamzaev on 8/10/24.
+//
+
+import SwiftUI
+
+@main
+struct GitSwiftfulThinkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
